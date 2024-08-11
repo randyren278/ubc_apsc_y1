e10f9df1a58c123d7f0cf4ad182267cac5dc6ae3
randyren278 <randyren278@gmail.com>
2024-08-11 12:58:01 -0700

    intial working globe demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { SidebarDemo } from "./components/bar";
+import { GlobeDemo } from "./components/globepage";
 
 export default function Home() {
   return (
     <div className="h-screen w-screen">
       <SidebarDemo />
+      <GlobeDemo/>
     </div>
   );
 }
