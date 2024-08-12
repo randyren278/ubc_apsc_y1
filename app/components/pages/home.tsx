9dbc57f6eede8c006c9501a6307366029d6c6cd6
randyren278 <randyren278@gmail.com>
2024-08-11 19:22:03 -0700

    preparation for file upload

--- app/components/pages/home.tsx
+++ app/components/pages/home.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <div className="p-8 bg-gray-100 dark:bg-neutral-900 min-h-screen">
       <h1 className="text-4xl font-bold text-black dark:text-white">
-        Welcome to APSC Buddy
+        Welcome to APSC Buddy 👋🏻
       </h1>
       <p className="mt-4 text-lg text-gray-600 dark:text-gray-300">
         Your best first-year engineering resource.
