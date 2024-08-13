bc49b60470b7adb68bc4fa0e4ab7d0c29f689008
randyren278 <randyren278@gmail.com>
2024-08-12 20:18:39 -0700

    edited side bar

--- app/components/bar.tsx
+++ app/components/bar.tsx
@@ -2,8 +2,6 @@
 import React, { useState } from "react";
 import {
   IconBook,
-  IconPencil,
-  IconCertificate,
   IconSchool,
   IconMail,
 } from "@tabler/icons-react";
@@ -20,15 +18,19 @@ export function SidebarDemo({ setTab }: SidebarDemoProps) {
     {
       label: "Courses",
       tab: "courses",
-      icon: <IconBook className="   text-neutral-200 h-5 w-5 flex-shrink-0" />,
+      icon: <IconBook className="text-neutral-200 h-5 w-5 flex-shrink-0" />,
     },
+    {
+      label: "Contact Me",
+      tab: "contact",
+      icon: <IconMail className="text-neutral-200 h-5 w-5 flex-shrink-0" />,
+      href: "mailto:randyren278@gmail.com"
+    }
   ];
 
   return (
     <div
-      className={`flex h-screen fixed${
-        isHovered ? "w-64" : "w-13"
-      }`}
+      className={`flex h-auto fixed${isHovered ? "w-64" : "w-13"}`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
@@ -47,44 +49,25 @@ export function SidebarDemo({ setTab }: SidebarDemoProps) {
             )}
           </div>
         </div>
-        <div className="mt-4 flex flex-col gap-2 flex-grow">
+        <div className="mt-1 flex flex-col gap-2 flex-grow">
           {links.map((link, idx) => (
             <div
               key={idx}
               onClick={() => setTab(link.tab)}
-              className="flex items-center p-4  hover:bg-neutral-700 rounded-md cursor-pointer"
+              className="flex items-center p-4 hover:bg-neutral-700 rounded-md cursor-pointer"
             >
               {link.icon}
               {isHovered && (
                 <motion.span
                   initial={{ opacity: 0 }}
                   animate={{ opacity: 1 }}
-                  className="ml-4    text-neutral-200 text-sm"
+                  className="ml-4 text-neutral-200 text-sm"
                 >
                   {link.label}
                 </motion.span>
               )}
             </div>
           ))}
-        </div>
-        <div className="mt-auto">
-          <div className="flex items-center p-4  hover:bg-neutral-700 rounded-md cursor-pointer">
-            <a
-              href="mailto:randyren278@gmail.com"
-              className="flex items-center  hover:bg-neutral-700 rounded-md cursor-pointer    text-neutral-200 hover:text-neutral-100"
-            >
-              <IconMail className="h-5 w-5 flex-shrink-0" />
-              {isHovered && (
-                <motion.span
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  className="ml-3    text-neutral-200 whitespace-pre text-sm"
-                >
-                  Contact Me
-                </motion.span>
-              )}
-            </a>
-          </div>
         </div>
       </div>
     </div>
